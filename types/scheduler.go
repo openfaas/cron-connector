9ef4fe234e261459bc8a084346b355e7504b2698
Rishabh Gupta <r.g.gupta@outlook.com>
2019-01-31 18:00:12 +0530

    Cleaned up code. Added new struct for scheduled functions. Divided main logic into functions
    Signed-off-by: Rishabh Gupta <r.g.gupta@outlook.com>

--- types/scheduler.go
+++ types/scheduler.go
@@ -16,13 +16,26 @@ type Scheduler struct {
 	main *cron.Cron
 }
 
+// ScheduledFunction is a CronFunction that has been scheduled to run
+type ScheduledFunction struct {
+
+	// Function is CronFunction object which is running
+	Function CronFunction
+
+	// Id is the entryid for the scheduled function
+	ID EntryID
+}
+
+// ScheduledFunctions is an array of ScheduledFunction
+type ScheduledFunctions []ScheduledFunction
+
 // AddCronFunction adds a function to cron
-func (s *Scheduler) AddCronFunction(c *CronFunction, invoker *types.Invoker) (EntryID, error) {
+func (s *Scheduler) AddCronFunction(c *CronFunction, invoker *types.Invoker) (ScheduledFunction, error) {
 	eID, err := s.main.AddFunc(c.Schedule, func() {
 		log.Print(fmt.Sprint("Executed function: ", c.Name))
 		c.InvokeFunction(invoker)
 	})
-	return EntryID(eID), err
+	return ScheduledFunction{*c, EntryID(eID)}, err
 }
 
 // NewScheduler returns a scheduler
@@ -38,12 +51,25 @@ func (s *Scheduler) Start() {
 }
 
 // Remove removes the function from scheduler
-func (s *Scheduler) Remove(id EntryID) {
-	s.main.Remove(cron.EntryID(id))
+func (s *Scheduler) Remove(function ScheduledFunction) {
+	s.main.Remove(cron.EntryID(function.ID))
 }
 
 // CheckSchedule returns true if the schedule string is compliant with cron
 func CheckSchedule(schedule string) bool {
 	_, err := cron.Parse(schedule)
 	return err == nil
+}
+
+// Contains returns true if the ScheduledFunctions array contains the CronFunction
+func (functions *ScheduledFunctions) Contains(cronFunc *CronFunction) bool {
+	for _, f := range *functions {
+
+		if f.Function.Name == cronFunc.Name && f.Function.Schedule == cronFunc.Schedule {
+			return true
+		}
+
+	}
+
+	return false
 }
