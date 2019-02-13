fefbf273899457691c89f01fb7cab4befb6bde7d
Rishabh Gupta <r.g.gupta@outlook.com>
2019-02-13 17:25:32 +0530

    Added arm build with linuxkit/binfmt
    Signed-off-by: Rishabh Gupta<r.g.gupta@outlook.com>

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker run --rm --privileged multiarch/qemu-user-static:register --reset
-docker login --username $DOCKER_USER --password $DOCKER_PASSWORD
-docker build -t zeerorg/cron-connector:$TRAVIS_TAG .
-docker push zeerorg/cron-connector:$TRAVIS_TAG
-docker build -t zeerorg/cron-connector:$TRAVIS_TAG-arm -f Dockerfile.armhf .
-docker push zeerorg/cron-connector:$TRAVIS_TAG-arm
+#docker login --username $DOCKER_USER --password $DOCKER_PASSWORD
+docker build -t zeerorg/cron-connector .
+#docker push zeerorg/cron-connector:$TRAVIS_TAG
+sudo docker run --privileged linuxkit/binfmt:v0.6
+docker build -t zeerorg/cron-connector:arm -f Dockerfile.armhf .
+#docker push zeerorg/cron-connector:$TRAVIS_TAG-arm
