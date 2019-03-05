d562e1160cbac97182d1691b190524702362d8c7
Rishabh Gupta <r.g.gupta@outlook.com>
2019-03-05 14:30:24 +0530

    Unified tagged build and normal build
    Signed-off-by: Rishabh Gupta <r.g.gupta@outlook.com>

--- travis/build-deploy.sh
+++ travis/build-deploy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Set image tag
+TAG=$TRAVIS_BRANCH
+if [ "$TRAVIS_PULL_REQUEST" -ne "false" ]
+then
+  TAG=$TRAVIS_PULL_REQUEST_BRANCH
+fi
+
 # Login into docker
 docker login --username $DOCKER_USER --password $DOCKER_PASSWORD
 
@@ -21,7 +28,7 @@ buildctl build --frontend dockerfile.v0 \
       --local dockerfile=. \
       --local context=. \
       --exporter image \
-      --exporter-opt name=docker.io/zeerorg/cron-connector:test-build \
+      --exporter-opt name=docker.io/zeerorg/cron-connector:$TAG \
       --exporter-opt push=true \
       --frontend-opt platform=$platforms \
       --frontend-opt filename=./Dockerfile.cross
@@ -34,7 +41,7 @@ do
       --local dockerfile=. \
       --local context=. \
       --exporter image \
-      --exporter-opt name=docker.io/zeerorg/cron-connector:test-build-$arch \
+      --exporter-opt name=docker.io/zeerorg/cron-connector:$TAG-$arch \
       --exporter-opt push=true \
       --frontend-opt platform=linux/$arch \
       --frontend-opt filename=./Dockerfile.cross &
@@ -42,6 +49,6 @@ done
 
 wait
 
-docker pull zeerorg/cron-connector:test-build-arm
-docker tag zeerorg/cron-connector:test-build-arm zeerorg/cron-connector:test-build-armhf
-docker push zeerorg/cron-connector:test-build-armhf
+docker pull zeerorg/cron-connector:$TAG-arm
+docker tag zeerorg/cron-connector:$TAG-arm zeerorg/cron-connector:$TAG-armhf
+docker push zeerorg/cron-connector:$TAG-armhf
